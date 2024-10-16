54a816c991c3070f79ef113dab8e2b49a1439ebd
SKC <146567010+keshavdsce@users.noreply.github.com>
2024-10-17 02:01:55 +0530

    update-01
    mini-change

--- whatsapp.py
+++ whatsapp.py
@@ -1,17 +1,5 @@
-from flask import Flask, request
+
 import pywhatkit
 
-app = Flask(__name__)
+pywhatkit.sendwhatmsg("+919483102675", "Hello Everybody", 9, 12)
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    msg_from = request.form.get('From')
-    msg_body = request.form.get('Body')
-
-    # Send a WhatsApp message using pywhatkit
-    pywhatkit.sendwhatmsg("+919483102675", "Hello", 00, 12)
-
-    return "Message received", 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
